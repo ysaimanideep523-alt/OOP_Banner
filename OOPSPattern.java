b047063380b4b362feaf47dd89ebc7c97eca7abb
ysaimanideep523-alt <ysaimanideep523@gmail.com>
2026-03-03 09:02:19 +0530

    uc5

--- OOPSPattern.java
+++ OOPSPattern.java
@@ -1,4 +1,4 @@
-public class OOPSBannerApp {
+public class OOPSPattern {
 	public static void main(String args[]){
 		String[] lines = new String[7];
     	lines[0] = "   ***   " + "  " + "   ***   " + "  " + "****** " + "  " + "   *****";
