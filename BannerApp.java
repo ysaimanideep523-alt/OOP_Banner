83f62d99dfb9f0d72e38ff299cbf9bd1c35a3345
ysaimanideep523-alt <ysaimanideep523@gmail.com>
2026-03-03 08:47:27 +0530

    uc4

--- BannerApp.java
+++ BannerApp.java
@@ -8,7 +8,5 @@ public class BannerApp{
 	    System.out.println(" **   ** " + "  " + " **   ** " + "  " + "**     " + "  " + "**   **");
 	    System.out.println("   ***   " + "  " + "   ***   " + "  " + "**     " + "  " + " *****");
 	   }
-}   
-	   
-		 
+}    
 	
